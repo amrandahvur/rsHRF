0f55eafc6fd55d0150f56f018c0ba1aa026dc575
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-08-07 14:42:39 +0530

    check

--- rsHRF/CLI.py
+++ rsHRF/CLI.py
@@ -5,7 +5,7 @@ import numpy as np
 import warnings
 from rsHRF import spm_dep, fourD_rsHRF
 
-__version__ = "0.1.9"
+__version__ = "0.1.11"
 
 def get_parser():
     parser = ArgumentParser(description='retrieves the onsets of pseudo-events triggering a '
